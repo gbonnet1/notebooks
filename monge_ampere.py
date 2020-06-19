6f9c24deb4dd8445f0f666df6bbd542ca29e9f51
Guillaume Bonnet <guillaume.bonnet@math.u-psud.fr>
2020-06-19 17:18:09 +0200

    Update code

--- monge_ampere.py
+++ monge_ampere.py
@@ -5,6 +5,8 @@
 
 
 # %%
+from dataclasses import dataclass
+
 import agd.LinearParallel as lp
 import matplotlib.pyplot as plt
 import numpy as np
@@ -67,15 +69,7 @@ We propose to discretize the last equation, which has the benefit that its solut
 # %% [markdown]
 """
 We discretize the reformulated equation on a grid $\mathcal{G}_h := \Omega \cap h \mathbb{Z}^2$, for some discretization step $h > 0$.
-"""
 
-
-# %%
-x = np.stack(np.meshgrid(*(2 * [np.linspace(-1, 1, 40)]), indexing="ij"))
-
-
-# %% [markdown]
-"""
 Let $u \colon \mathcal{G}_h \to \mathbb{R}$ and $x \in \mathcal{G}_h$. If it makes sense (we discuss handling of boundary conditions in later sections), we define
 \begin{equation}
     D_h u(x) := \frac{1}{2 h} \begin{pmatrix}
@@ -99,8 +93,8 @@ For any such family $v$, we define $\mathcal{S}^+(v) \subset \mathcal{S}_2^+$ as
 The *stencil* of the numerical scheme is described by three sets $V_1 \subset \mathbb{Z}^2$, $V_2 \subset (\mathbb{Z}^2)^2$, and $V_3 \subset (\mathbb{Z}^2)^3$ such that
 
 * Any family $v \in V_3$ is a superbase of $\mathbb{Z}^2$, meaning that $\det(v_1, v_2) = \pm 1$ and $v_1 + v_2 + v_3 = 0$. Note that this also implies that $\det(v_2, v_3) = \det(v_3, v_1) = \pm 1$.
-* Any family $v \in V_2$ is a basis of $\mathbb{Z}^2$, meaning that $\det(v_1, v_2) = \pm 1$, and moreover $\bigcup_{v \in V_2} \{v_1, v_2\} = \bigcup_{v \in v_3} \bigcup_{1 \leq i < j \leq 3} \{v_i, v_j\}$.
-* Any vector $v \in V_1$ is irreductible, meaning that its components are coprime, and moreover $V_1 = \bigcup_{v \in V_2} (v_1 \cup v_2)$.
+* Any family $v \in V_2$ is a basis of $\mathbb{Z}^2$, meaning that $\det(v_1, v_2) = \pm 1$, and moreover $\bigcup_{v \in V_2} \{\pm v_1, \pm v_2\} = \bigcup_{v \in v_3} \bigcup_{1 \leq i < j \leq 3} \{\pm v_i, \pm v_j\}$.
+* Any vector $v \in V_1$ is irreductible, meaning that its components are coprime, and moreover $\bigcup_{v \in V_1} \{\pm v\} = \bigcup_{v \in V_2} (\{\pm v_1\} \cup \{\pm v_2\})$.
 
 For any superbase $v \in V_3$ and matrix $\mathcal{D} \in \mathcal{S}_2$, the coordinates of $\mathcal{D}$ in the basis $(v_1 v_1^\top, v_2 v_2^\top, v_3 v_3^\top)$ of $\mathcal{S}_2$ are given by *Selling's formula*:
 \begin{equation}
@@ -142,40 +136,48 @@ where
 \begin{equation}
     W := \frac{1}{2} \begin{pmatrix}
         v_{2,1} v_{3,1} - v_{2,2} v_{3,2} & v_{2,1} v_{3,2} + v_{2,2} v_{3,1} \\
-        v_{3,1} v_{1,1} - v_{3,2} v_{1,2} & v_{3,1} v_{1,2} + v_{3,2} v_{1,1} \\
+        v_{1,1} v_{3,1} - v_{1,2} v_{3,2} & v_{1,1} v_{3,2} + v_{1,2} v_{3,1} \\
         v_{1,1} v_{2,1} - v_{1,2} v_{2,2} & v_{1,1} v_{2,2} + v_{1,2} v_{2,1}
-    \end{pmatrix}
+    \end{pmatrix},
 \end{equation}
-and
 \begin{equation}
     w := -\frac{1}{2} \begin{pmatrix}
         \langle v_2, v_3 \rangle \\
-        \langle v_3, v_1 \rangle \\
+        \langle v_1, v_3 \rangle \\
         \langle v_1, v_2 \rangle
     \end{pmatrix}.
 \end{equation}
-Therefore the definition of $H$ may be rewritten as
+Remark that the expression of the matrix $Q := W W^\top$ simplifies to
 \begin{equation}
-    H(v, b, \delta) = \max_{\substack{\rho \in \overline B(0, 1) \\ W \rho + w \geq 0}} \left(b^{1/2} (1 - |\rho|^2)^{1/2} - \langle W \rho + w, \delta \rangle\right).
+    Q = \frac{1}{4} \begin{pmatrix}
+        |v_2|^2 |v_3|^2 & \langle v_1, v_2 \rangle |v_3|^2 & \langle v_1, v_3 \rangle |v_2|^2 \\
+        \langle v_1, v_2 \rangle |v_3|^2 & |v_1|^2 |v_3|^2 & \langle v_2, v_3 \rangle |v_1|^2 \\
+        \langle v_1, v_3 \rangle |v_2|^2 & \langle v_2, v_3 \rangle |v_1|^2 & |v_1|^2 |v_2|^2
+    \end{pmatrix}.
 \end{equation}
-
-If the optimal $\rho$ is such that $W \rho + w > 0$ elementwise, then by convexity,
+The definition of $H$ may be rewritten as
 \begin{equation}
-    H(v, b, \delta) = \max_{\rho \in \overline B(0, 1)} \left(b^{1/2} (1 - |\rho|^2)^{1/2} - \langle W \rho + w, \delta \rangle\right) = (b + |W^\top \delta|^2)^{1/2} - \langle w, \delta \rangle,
+    H(v, b, \delta) = \max_{\substack{\rho \in \overline B(0, 1) \\ W \rho + w \geq 0}} \left(b^{1/2} (1 - |\rho|^2)^{1/2} - \langle W \rho + w, \delta \rangle\right),
+\end{equation}
+where the inequality $W \rho + w \geq 0$ must hold elementwise.
+
+If the optimal $\rho$ is such that $W \rho + w > 0$, then by convexity,
+\begin{equation}
+    H(v, b, \delta) = \max_{\rho \in \overline B(0, 1)} \left(b^{1/2} (1 - |\rho|^2)^{1/2} - \langle W \rho + w, \delta \rangle\right) = (b + \langle \delta, Q \delta \rangle)^{1/2} - \langle w, \delta \rangle,
 \end{equation}
 and the maximum is attained at point
 \begin{equation}
-    \rho_*(v, b, \delta) := -\frac{W^\top \delta}{(b + |W^\top \delta|^2)^{1/2}}.
+    \rho_*(v, b, \delta) := -\frac{W^\top \delta}{(b + \langle \delta, Q \delta \rangle)^{1/2}}.
 \end{equation}
 
-To handle cases where the constraint $W \rho + w$ is saturated, we rewrite the numerical scheme as
+Note that the inequality $W \rho_*(v, b, \delta) \geq 0$ is equivalent to $Q \delta \leq (b + \langle \delta, Q \delta \rangle)^{1/2} w$. To handle cases where the constraint $W \rho + w \geq 0$ is saturated in the definition of $H$, we rewrite the numerical scheme as
 \begin{equation}
     \max_{i \in \{2, 3\}} \max_{v \in V_i} H_i(v, B(x, u(x), D_h u(x)), \Delta_h^v [u, A(x, u(x), D_h u(x))](x)) = 0,
 \end{equation}
 where
 \begin{equation}
     H_3(v, b, \delta) := \begin{cases}
-        (b + |W^\top \delta|^2)^{1/2} - \langle w, \delta \rangle &\text{if } W \rho_*(v, b, \delta) + w > 0, \\
+        (b + \langle \delta, Q \delta \rangle)^{1/2} - \langle w, \delta \rangle &\text{if } Q \delta \leq (b + \langle \delta, Q \delta \rangle)^{1/2} w, \\
         -\infty &\text{else},
     \end{cases}
 \end{equation}
@@ -192,254 +194,210 @@ The maximum in the definition of $H_2$ may also be computed exactly: for any $v 
 \end{equation}
 We deduce that
 \begin{equation}
-    H_2(v, b, \delta) = \left(\frac{b}{|v_1| |v_2|} + \left(\frac{\delta_1}{2 |v_1|^2} - \frac{\delta_2}{2 |v_2|^2}\right)^2\right)^{1/2} - \left(\frac{\delta_1}{2 |v_1|^2} + \frac{\delta_2}{2 |v_2|^2}\right).
+    H_2(v, b, \delta) = \left(\frac{b}{|v_1|^2 |v_2|^2} + \left(\frac{\delta_1}{2 |v_1|^2} - \frac{\delta_2}{2 |v_2|^2}\right)^2\right)^{1/2} - \left(\frac{\delta_1}{2 |v_1|^2} + \frac{\delta_2}{2 |v_2|^2}\right),
+\end{equation}
+which we write as
+\begin{equation}
+    H_2(v, b, \delta) = (\omega_0 b + \langle \omega_1, \delta \rangle^2)^{1/2} - \langle \omega_2, \delta \rangle
+\end{equation}
+where
+\begin{equation}
+    \omega_0 := \frac{1}{|v_1|^2 |v_2|^2},
+\end{equation}
+\begin{equation}
+    \omega_1 := \frac{1}{2} \begin{pmatrix}
+        1 / |v_1|^2 \\
+        -1 / |v_2|^2
+    \end{pmatrix},
+\end{equation}
+\begin{equation}
+    \omega_3 := \frac{1}{2} \begin{pmatrix}
+        1 / |v_1|^2 \\
+        1 / |v_2|^2
+    \end{pmatrix}.
 \end{equation}
 """
 
 
-# %%
-superbases = Selling.SuperbasesForConditioning(15)
+# %% [markdown]
+"""
+3. Implementation of the scheme
+"""
 
 
 # %%
-def MA(A, B, d2u, superbases):
-    superbases = np.expand_dims(superbases, (3, 4))
+x = np.stack(np.meshgrid(*(2 * [np.linspace(-1, 1, 40)]), indexing="ij"))
+domain_ball = Domain.Ball(radius=0.9)
 
-    delta = d2u - lp.dot_VAV(
-        lp.perp(superbases), A[:, :, np.newaxis, np.newaxis], lp.perp(superbases)
-    )
 
-    B_zero = B == 0
-    B = np.where(B_zero, 1, B)
+# %%
+@dataclass
+class Stencil:
+    V1: np.ndarray
+    V2: np.ndarray
+    V2_indices: np.ndarray
+    V3: np.ndarray
+    V3_indices: np.ndarray
+    Q: np.ndarray
+    w: np.ndarray
+    omega0: np.ndarray
+    omega1: np.ndarray
+    omega2: np.ndarray
 
-    residue = -np.inf
 
-    W = (
-        -np.stack(
-            [
-                np.roll(superbases[0], 1, axis=0) * np.roll(superbases[0], 2, axis=0)
-                - np.roll(superbases[1], 1, axis=0) * np.roll(superbases[1], 2, axis=0),
-                np.roll(superbases[0], 1, axis=0) * np.roll(superbases[1], 2, axis=0)
-                + np.roll(superbases[1], 1, axis=0) * np.roll(superbases[0], 2, axis=0),
-            ]
-        )
-        / 2
-    )
-    w = -lp.dot_VV(np.roll(superbases, 1, axis=1), np.roll(superbases, 2, axis=1)) / 2
+# %%
+def StencilForConditioning(cond):
+    V3 = Selling.SuperbasesForConditioning(cond)
+    offsets = V3.reshape((2, -1))
 
-    q = lp.dot_AV(W, delta)
-    r = np.sqrt(B + lp.dot_VV(q, q))
+    # Make offsets positive for the lexicographic order, inversing their sign if needed.
+    offsets[:, offsets[0] < 0] *= -1
+    offsets[:, np.logical_and(offsets[0] == 0, offsets[1] < 0)] *= -1
 
-    residue = np.maximum(
-        residue,
-        np.max(
-            np.where(
-                np.all(lp.dot_AV(lp.transpose(W), q) <= r * w, axis=0),
-                r - lp.dot_VV(w, delta),
-                -np.inf,
+    V1, indices = np.unique(offsets, axis=1, return_inverse=True)
+    V3_indices = indices.reshape(V3.shape[1:])
+    V2_indices = np.unique(
+        np.sort(
+            np.concatenate(
+                (V3_indices[[0, 1]], V3_indices[[0, 2]], V3_indices[[1, 2]]), axis=1
             ),
             axis=0,
         ),
+        axis=1,
+    )
+    V2 = V1[:, V2_indices]
+
+    Q = np.zeros((3, 3, V3.shape[2]))
+    w = np.zeros((3, V3.shape[2]))
+
+    for i in range(3):
+        Q[i, i] = (
+            lp.dot_VV(V3[:, (i + 1) % 3], V3[:, (i + 1) % 3])
+            * lp.dot_VV(V3[:, (i + 2) % 3], V3[:, (i + 2) % 3])
+            / 4
+        )
+        Q[i, (i + 1) % 3] = (
+            lp.dot_VV(V3[:, i], V3[:, (i + 1) % 3])
+            * lp.dot_VV(V3[:, (i + 2) % 3], V3[:, (i + 2) % 3])
+            / 4
+        )
+        Q[i, (i + 2) % 3] = (
+            lp.dot_VV(V3[:, i], V3[:, (i + 2) % 3])
+            * lp.dot_VV(V3[:, (i + 1) % 3], V3[:, (i + 1) % 3])
+            / 4
+        )
+        w[i] = -lp.dot_VV(V3[:, (i + 1) % 3], V3[:, (i + 2) % 3]) / 2
+
+    omega0 = 1 / (lp.dot_VV(V2[:, 0], V2[:, 0]) * lp.dot_VV(V2[:, 1], V2[:, 1]))
+    omega1 = 1 / (
+        2 * np.stack([lp.dot_VV(V2[:, 0], V2[:, 0]), -lp.dot_VV(V2[:, 1], V2[:, 1])])
+    )
+    omega2 = 1 / (
+        2 * np.stack([lp.dot_VV(V2[:, 0], V2[:, 0]), lp.dot_VV(V2[:, 1], V2[:, 1])])
     )
 
-    bases = np.concatenate(
-        [superbases[:, [0, 1]], superbases[:, [0, 2]], superbases[:, [1, 2]]], axis=2
-    )
-    delta_bases = np.concatenate([delta[[0, 1]], delta[[0, 2]], delta[[1, 2]]], axis=1)
+    return Stencil(V1, V2, V2_indices, V3, V3_indices, Q, w, omega0, omega1, omega2)
 
-    residue = np.maximum(
-        residue,
-        np.max(
-            np.sqrt(
-                B
-                * lp.det(bases) ** 2
-                / (
-                    lp.dot_VV(bases[:, 0], bases[:, 0])
-                    * lp.dot_VV(bases[:, 1], bases[:, 1])
-                )
-                + (
-                    delta_bases[0] / lp.dot_VV(bases[:, 0], bases[:, 0])
-                    - delta_bases[1] / lp.dot_VV(bases[:, 1], bases[:, 1])
-                )
-                ** 2
-                / 4
-            )
-            - (
-                delta_bases[0] / lp.dot_VV(bases[:, 0], bases[:, 0])
-                + delta_bases[1] / lp.dot_VV(bases[:, 1], bases[:, 1])
-            )
-            / 2,
-            axis=0,
+
+# %%
+stencil = StencilForConditioning(15)
+
+
+# %%
+def H3(b, delta, stencil):
+    Q = np.expand_dims(stencil.Q, (3, 4))
+    w = np.expand_dims(stencil.w, (2, 3))
+    delta = delta[stencil.V3_indices]
+
+    Q_delta = lp.dot_AV(Q, delta)
+    r = np.sqrt(b + lp.dot_VV(delta, Q_delta))
+    return np.where(np.all(Q_delta <= r * w, axis=0), r - lp.dot_VV(w, delta), -np.inf)
+
+
+# %%
+def H2(b, delta, stencil):
+    omega0 = np.expand_dims(stencil.omega0, (1, 2))
+    omega1 = np.expand_dims(stencil.omega1, (2, 3))
+    omega2 = np.expand_dims(stencil.omega2, (2, 3))
+    delta = delta[stencil.V2_indices]
+
+    return np.sqrt(omega0 * b + lp.dot_VV(omega1, delta) ** 2) - lp.dot_VV(
+        omega2, delta
+    )
+
+
+# %%
+def H1(delta, stencil):
+    return -delta / np.expand_dims(lp.dot_VV(stencil.V1, stencil.V1), (1, 2))
+
+
+# %%
+def Scheme(a, b, d2u, stencil):
+    delta = d2u - lp.dot_VAV(
+        np.expand_dims(stencil.V1, (2, 3)),
+        np.expand_dims(a, 2),
+        np.expand_dims(stencil.V1, (2, 3)),
+    )
+
+    b_zero = b == 0
+
+    return np.where(
+        b_zero,
+        np.max(H1(delta, stencil), axis=0),
+        np.maximum(
+            np.max(H2(np.where(b_zero, 1, b), delta, stencil), axis=0),
+            np.max(H3(np.where(b_zero, 1, b), delta, stencil), axis=0),
         ),
     )
 
-    residue = np.where(B_zero, np.max(np.max(-delta, axis=0), axis=0), residue)
-
-    return residue
-
 
 # %% [markdown]
 """
-## 3. Dirichlet boundary conditions
+4. Dirichlet boundary conditions
 """
 
 
 # %%
-def SchemeDirichlet(u, x, domain, A, B, g, superbases):
+def SchemeDirichlet(u, x, domain, A, B, g, stencil):
     bc = Domain.Dirichlet(domain, g, x)
 
     du = bc.DiffCentered(u, [[1, 0], [0, 1]])
-    d2u = bc.Diff2(u, lp.perp(superbases))
+    d2u = bc.Diff2(u, stencil.V1)
 
     return np.where(
-        bc.interior, MA(A(x, u, du), B(x, u, du), d2u, superbases), u - bc.grid_values
+        bc.interior, Scheme(A(x, u, du), B(x, u, du), d2u, stencil), u - bc.grid_values
     )
 
 
 # %%
-domain = Domain.Box([[-1, 1], [-1, 1]])
+# TODO: bug in agd freeway?
 
 
-def A(x, r, p):
+def A_zero(x, r, p):
     return np.zeros((2, 2) + x.shape[1:])
 
 
-def B(x, r, p):
-    return np.ones(x.shape[1:])
-
-
-u = newton_root(
-    SchemeDirichlet, np.zeros(x.shape[1:]), (x, domain, A, B, 0.0, superbases)
-)
-u = np.where(domain.level(x) < 0, u, np.nan)
-
-plt.contourf(*x, u)
-plt.show()
-
-
-# %% [markdown]
-"""
-### 3.1. Comparison with the exact solution
-"""
-
-
-# %%
-domain = Domain.Ball()
-
-
-def A(x, r, p):
-    return np.zeros((2, 2) + x.shape[1:])
-
-
-def B(x, r, p):
+def B_quartic(x, r, p):
     return 48 * lp.dot_VV(x, x) ** 2
 
 
-def Exact(x):
+u = newton_root(
+    SchemeDirichlet,
+    np.zeros(x.shape[1:]),
+    (x, domain_ball, A_zero, B_quartic, 0.9 ** 4, stencil),
+)
+u = np.where(domain_ball.level(x) < 0, u, np.nan)
+
+plt.contourf(*x, u)
+plt.show()
+
+
+def ExactQuartic(x):
     return lp.dot_VV(x, x) ** 2
 
 
-u = newton_root(
-    SchemeDirichlet, np.zeros(x.shape[1:]), (x, domain, A, B, 1.0, superbases)
-)
-u = np.where(domain.level(x) < 0, u, np.nan)
-
-plt.contourf(*x, u)
-plt.show()
-
-err = np.where(domain.level(x) < 0, u - Exact(x), 0)
-print("Error:", np.max(np.abs(err)))
-
-
-# %%
-Q = np.array([[2, 1], [1, 2]])
-Q_inv = np.array([[2, -1], [-1, 2]]) / 3
-
-assert np.allclose(Q @ Q_inv, np.eye(2))
-
-domain = Domain.Ball()
-
-
-def A(x, r, p):
-    return -(
-        r / 3 + lp.dot_VAV(p, np.multiply.outer(Q_inv, np.ones(x.shape[1:])), p) / 3
-    ) * np.multiply.outer(Q, np.ones(x.shape[1:]))
-
-
-def B(x, r, p):
-    return (
-        3
-        * (
-            1
-            + 2 * r / 3
-            + lp.dot_VAV(p, np.multiply.outer(Q_inv, np.ones(x.shape[1:])), p) / 6
-        )
-        ** 2
-    )
-
-
-def Exact(x):
-    return lp.dot_VAV(x, np.multiply.outer(Q, np.ones(x.shape[1:])), x) / 2
-
-
-u = newton_root(
-    SchemeDirichlet, np.zeros(x.shape[1:]), (x, domain, A, B, Exact, superbases)
-)
-u = np.where(domain.level(x) < 0, u, np.nan)
-
-plt.contourf(*x, u)
-plt.show()
-
-err = np.where(domain.level(x) < 0, u - Exact(x), 0)
-print("Error:", np.max(np.abs(err)))
-
-
-# %% [markdown]
-"""
-### 3.2. Other domains
-"""
-
-
-# %%
-domain = Domain.Union(Domain.Ball(), Domain.Box())
-
-
-def A(x, r, p):
-    return np.zeros((2, 2) + x.shape[1:])
-
-
-def B(x, r, p):
-    return np.ones(x.shape[1:])
-
-
-u = newton_root(
-    SchemeDirichlet, np.zeros(x.shape[1:]), (x, domain, A, B, 0.0, superbases)
-)
-u = np.where(domain.level(x) < 0, u, np.nan)
-
-plt.contourf(*x, u)
-plt.show()
-
-
-# %%
-domain = Domain.Complement(Domain.Ball(), Domain.Box())
-
-
-def A(x, r, p):
-    return np.zeros((2, 2) + x.shape[1:])
-
-
-def B(x, r, p):
-    return np.ones(x.shape[1:])
-
-
-u = newton_root(
-    SchemeDirichlet, np.zeros(x.shape[1:]), (x, domain, A, B, 0.0, superbases)
-)
-u = np.where(domain.level(x) < 0, u, np.nan)
-
-plt.contourf(*x, u)
-plt.show()
+error = np.where(domain_ball.level(x) < 0, u - ExactQuartic(x), 0)
+print("Error:", np.max(np.abs(error)))
 
 
 # %% [markdown]
@@ -449,7 +407,7 @@ plt.show()
 
 
 # %%
-def SchemeBV2(u, x, domain, A, B, C, sigma, superbases):
+def SchemeBV2(u, x, domain, A, B, C, sigma, stencil):
     bc = Domain.Dirichlet(Domain.Box([[-1, 1], [-1, 1]]), np.inf, x)
 
     du0 = bc.DiffUpwind(u, [[1, 0], [0, 1]])
@@ -459,23 +417,23 @@ def SchemeBV2(u, x, domain, A, B, C, sigma, superbases):
     du1 = np.where(du1 == np.inf, -du0, du1)
     du = (du0 - du1) / 2
 
-    du0 = bc.DiffUpwind(u, lp.perp(superbases))
+    du0 = bc.DiffUpwind(u, stencil.V1)
     du0 = np.where(
         du0 == np.inf,
         sigma(
-            x[:, np.newaxis, np.newaxis],
-            u[np.newaxis, np.newaxis],
-            np.multiply.outer(lp.perp(superbases), np.ones(x.shape[1:])),
+            np.expand_dims(x, 1),
+            np.expand_dims(u, 0),
+            np.multiply.outer(stencil.V1, np.ones(x.shape[1:])),
         ),
         du0,
     )
-    du1 = bc.DiffUpwind(u, -lp.perp(superbases))
+    du1 = bc.DiffUpwind(u, -stencil.V1)
     du1 = np.where(
         du1 == np.inf,
         sigma(
-            x[:, np.newaxis, np.newaxis],
-            u[np.newaxis, np.newaxis],
-            np.multiply.outer(-lp.perp(superbases), np.ones(x.shape[1:])),
+            np.expand_dims(x, 1),
+            np.expand_dims(u, 0),
+            np.multiply.outer(-stencil.V1, np.ones(x.shape[1:])),
         ),
         du1,
     )
@@ -483,81 +441,38 @@ def SchemeBV2(u, x, domain, A, B, C, sigma, superbases):
 
     return np.where(
         bc.interior,
-        MA(A(x, u, du), np.where(domain.level(x) < 0, B(x, u, du), 0), d2u, superbases)
+        Scheme(A(x, u, du), np.where(domain.level(x) < 0, B(x, u, du), 0), d2u, stencil)
         + u.flatten()[np.argmin(domain.level(x))]
         - C,
         u - bc.grid_values,
     )
 
 
-# %% [markdown]
-"""
-### 4.1. Comparison with the exact solution
-"""
-
-
 # %%
-domain = Domain.Ball(radius=0.9)
-
-
-def A(x, r, p):
-    return np.zeros((2, 2) + x.shape[1:])
-
-
-def B(x, r, p):
-    return 48 * lp.dot_VV(x, x) ** 2
-
-
-def sigma(x, r, e):
-    return 4 * 0.9 ** 3 * np.sqrt(lp.dot_VV(e, e))
-
-
-def Exact(x):
-    return lp.dot_VV(x, x) ** 2
-
-
-u = newton_root(SchemeBV2, lp.dot_VV(x, x), (x, domain, A, B, 0, sigma, superbases))
-u = np.where(domain.level(x) < 0, u, np.nan)
-
-plt.contourf(*x, u)
-plt.show()
-
-err = np.where(domain.level(x) < 0, u - Exact(x), 0)
-print("Error:", np.max(np.abs(err)))
-
-
-# %% [markdown]
-"""
-### 4.2. Near-field reflector design
-"""
-
-
-# %%
-domain = Domain.Ball(radius=0.9)
-
-
 def f(x):
     return np.full(x.shape[1:], 1 / 0.9 ** 2)
 
 
-def A(x, r, p):
+def A_reflector(x, r, p):
     tmp = 1 + np.sqrt(1 - lp.dot_VV(p, p) / r ** 4)
     return (2 + tmp) / r * lp.outer(p, p) - r ** 3 * tmp * lp.identity(x.shape[1:])
 
 
-def B(x, r, p):
+def B_reflector(x, r, p):
     tmp = 1 + np.sqrt(1 - lp.dot_VV(p, p) / r ** 4)
     return r ** 6 * (tmp ** 3 - tmp ** 2) * f(x)
 
 
-def sigma(x, r, e):
+def sigma_reflector(x, r, e):
     return 2 * r ** 3 * (np.sqrt(lp.dot_VV(e, e)) - lp.dot_VV(x, e))
 
 
 u = newton_root(
-    SchemeBV2, np.full(x.shape[1:], 0.1), (x, domain, A, B, 0.1, sigma, superbases),
+    SchemeBV2,
+    np.full(x.shape[1:], 0.1),
+    (x, domain_ball, A_reflector, B_reflector, 0.1, sigma_reflector, stencil),
 )
-u = np.where(domain.level(x) < 0, u, np.nan)
+u = np.where(domain_ball.level(x) < 0, u, np.nan)
 
 plt.contourf(*x, u)
 plt.show()
